34a7265a80350d4fb9a9a9f0968b29cd6a5b2315
Bryan Knight <bjknight1980@gmail.com>
2015-01-21 13:24:34 -0800

    initial commit

---
+++ app/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+	Route::get('/', function()
+	{
+		return View::make('index');
+	});
+
+	Route::get('todos', function()
+	{
+		return Todo::all();
+	});
+
+	Route::post('todos', function()
+	{
+		return Todo::create(Input::all());
+	});
+
+
